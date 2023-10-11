1dff6dba07e0bb64c96425b51a448f4fdb493549
Abner <2414782779@qq.com>
2023-10-12 03:26:38 +0800

    webpack-dev-server

--- webpack2/webpack.config.js
+++ webpack2/webpack.config.js
@@ -2,18 +2,26 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "管理输出",
+      title: 'Development',
     }),
   ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true, // 每次构建清空dist目录
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
